bb48d06e7a778e08d2fc1ebf1162850fb0e9c85b
EsseivaN <nicolas.esseiva@gmail.com>
2023-02-12 17:38:23 +0100

    changed from src to renderer + imported files

--- main.js
+++ main.js
@@ -11,13 +11,10 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-    },
   });
 
   // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'src/index.html'));
+  mainWindow.loadFile(path.join(__dirname, 'renderer/index.html'));
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
