f2fbc6ca8eab6f405b7d869499d0dbe2bad26ffb
EsseivaN <nicolas.esseiva@gmail.com>
2023-02-12 17:52:54 +0100

    get image width and height

--- renderer/js/renderer.js
+++ renderer/js/renderer.js
@@ -14,6 +14,14 @@ function loadImage(e) {
     return;
   }
 
+  // Get original dimensions
+  const image = new Image();
+  image.src = URL.createObjectURL(file);
+  image.onload = function () {
+    widthInput.value = this.width;
+    heightInput.value = this.height;
+  };
+
   form.style.display = 'block'; // shows the form
   filenameSpan.innerText = file.name;
 }
