a82e2a33753289b361a369cef8a8891910c8f23b
EsseivaN <nicolas.esseiva@gmail.com>
2023-02-12 17:42:43 +0100

    Added custom file menu

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
 const path = require('path');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
@@ -23,7 +23,34 @@ const createWindow = () => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('ready', () => {
+  createWindow();
+
+  // Implement menu
+  const mainMenu = Menu.buildFromTemplate(menu);
+  Menu.setApplicationMenu(mainMenu);
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  })
+});
+
+// Custom Menu template
+const menu = [
+  {
+    //role: 'fileMenu', // Default File menu and submenus
+    label: 'File',
+    submenu: [
+      {
+        label: 'Quit',
+        click: app.quit,
+        accelerator: 'CmdOrCtrl+W',
+      }
+    ]
+  },
+]
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
